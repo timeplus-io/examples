a009e7ca59b216be793ebe82dbcd3987e815affe
Ubuntu <ubuntu@ip-172-31-17-205.us-west-2.compute.internal>
2025-03-03 17:43:54 +0000

    update linux stack

--- open-telemetry/linux/script/streams.sql
+++ open-telemetry/linux/script/streams.sql
@@ -1,7 +1,15 @@
-CREATE STREAM default.otel
+CREATE STREAM default.otel_logs
 (
   `raw` string
 )
 ENGINE = ExternalStream
-SETTINGS type = 'kafka', brokers = 'redpanda:9092', topic = 'otel-data', security_protocol = 'PLAINTEXT', data_format = 'RawBLOB', skip_ssl_cert_check = 'false', one_message_per_row = 'true'
-COMMENT 'otel collected container logs and metrics'
+SETTINGS type = 'kafka', brokers = 'redpanda:9092', topic = 'otel-logs', security_protocol = 'PLAINTEXT', data_format = 'RawBLOB', skip_ssl_cert_check = 'false', one_message_per_row = 'true'
+COMMENT 'otel collected container logs';
+
+CREATE STREAM default.otel_metrics
+(
+  `raw` string
+)
+ENGINE = ExternalStream
+SETTINGS type = 'kafka', brokers = 'redpanda:9092', topic = 'otel-metrics', security_protocol = 'PLAINTEXT', data_format = 'RawBLOB', skip_ssl_cert_check = 'false', one_message_per_row = 'true'
+COMMENT 'otel collected container metrics';
